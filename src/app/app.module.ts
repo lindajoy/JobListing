986da6f0dcd938806c0bd71853601cdd001281b6
lindajoy <wawirajoy98@gmail.com>
2023-11-25 13:02:47 +0300

    chore: Updated app Module with the latest modules for state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import  { JobReducer } from './jobs/store/jobs.reducer';
+import  { JobsEffect  } from './jobs/store/jobs.effect';
+import {  HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -11,9 +19,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    NgbModule
+    BrowserModule,
+    NgbModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    EffectsModule.forFeature([JobsEffect]),
+
+    StoreModule.forFeature('joblistings', JobReducer),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [],
   bootstrap: [AppComponent]
