ff276b3a675126bbc7987813a9118c5093c3c7ab
Linda Joy <wawirajoy98@gmail.com>
2024-09-15 09:16:04 +0000

    HELLO

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
